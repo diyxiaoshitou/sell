94a7450d0ee56a1d5ffb00fb9a60e38122f52df1
张俊锋 <zhang.junfeng@rhxtune.com>
2017-02-27 01:07:54 +0800

    update

---
+++ src/common/js/date.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @created on : 2017/2/25,
+ * @author     : zhang.junfeng[zhang.junfeng@rhxtune.com]
+ * @version    : 1.0
+ * @copyright  : Copyright(c) 2016 北京瑞华信通信息技术有限公司
+ *
+ */
+    
